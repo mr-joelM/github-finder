87fc19511b87b256ad6f87d12deb467aae72cad4
mr-joelM <meyerjoel01@gmail.com>
2021-11-23 15:14:47 +0000

    component Users added.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -9,7 +9,7 @@ class UserItem extends Component {
   }
 
   render() {
-    const { id, login, avatar_url, html_url } = this.state
+    const { id, login, avatar_url, html_url } = this.props.user
 
     return (
       <div className="card text-center">
