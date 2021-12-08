de6b211e391c89610b15ba2d7ea8d26250bc6952
mr-joelM <meyerjoel01@gmail.com>
2021-12-08 10:59:10 +0000

    setup of Loading spinner

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import spinner from './spinner.gif'
+
+export const Spinner = () => {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        alt="Loading..."
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </Fragment>
+  )
+}
