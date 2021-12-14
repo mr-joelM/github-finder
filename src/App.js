b3b7dfd38e7ce8fd3deb4edf32d7d21d8f11808b
mr-joelM <meyerjoel01@gmail.com>
2021-12-14 11:49:47 +0000

    react-router-dom and Link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from 'react-router-dom'
+// In react-router-dom update v6, "Switch" is replaced by routes "Routes" and Component by element.
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import Users from './components/users/Users'
+import User from './components/users/User'
+import Search from './components/users/Search'
+import About from './components/pages/About'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -22,6 +31,17 @@ class App extends Component {
     // console.log(res)
 
     this.setState({ users: res.data.items, loading: false })
+  }
+
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    // console.log(res)
+
+    this.setState({ user: res.data, loading: false })
   }
 
   //Clear Users from state
@@ -38,22 +58,47 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state
+    const { users, user, loading } = this.state
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
