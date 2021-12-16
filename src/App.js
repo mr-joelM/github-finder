a761905c284af84043df872caedbfad8936d3d9a
mr-joelM <meyerjoel01@gmail.com>
2021-12-16 18:35:36 +0000

    layout of Repos and RepoItem to User and App

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   }
 
   //Search Github Users
@@ -44,6 +45,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false })
   }
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    // console.log(res)
+
+    this.setState({ repos: res.data, loading: false })
+  }
+
   //Clear Users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false })
@@ -58,7 +70,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state
+    const { users, user, repos, loading } = this.state
 
     return (
       <Router>
@@ -90,7 +102,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
