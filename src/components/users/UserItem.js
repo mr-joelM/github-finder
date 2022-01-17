e7f2a12d07f02c4461f377e61d0d16bc8ac1cfbb
mr-joelM <meyerjoel01@gmail.com>
2022-01-17 13:00:29 +0000

    css changes for mobile compatibility

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 const UserItem = ({ user: { login, avatar_url } }) => {
@@ -20,9 +19,5 @@ const UserItem = ({ user: { login, avatar_url } }) => {
     </div>
   )
 }
-
-// UserItem.propTypes = {
-//   user: PropTypes.object.isRequired,
-// }
 
 export default UserItem
